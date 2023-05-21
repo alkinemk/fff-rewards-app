b8a7722b2a5d23512e09100573867cbe471bb105
alkinemk <mazatkelian@yahoo.fr>
2023-05-21 14:42:59 +0200

    more testing

--- frontend/src/components/WalletInput.tsx
+++ frontend/src/components/WalletInput.tsx
@@ -16,9 +16,7 @@ const getMissionsRewards = (walletList: Array<string>, mode: string) => {
   const params = new URLSearchParams();
   walletList.forEach((address) => params.append("walletList", address));
   params.append("mode", mode);
-  const response = fetch(
-    `https://198.199.79.252:8000/api/mission_rewards?${params.toString()}`
-  );
+  const response = fetch(`/api/mission_rewards?${params.toString()}`);
   return response;
 };
 
@@ -26,9 +24,7 @@ const getStakingRewards = (walletList: Array<string>, mode: string) => {
   const params = new URLSearchParams();
   walletList.forEach((address) => params.append("walletList", address));
   params.append("mode", mode);
-  const response = fetch(
-    `https://198.199.79.252:8000/api/staking_rewards?${params.toString()}`
-  );
+  const response = fetch(`/api/staking_rewards?${params.toString()}`);
   return response;
 };
 
@@ -36,9 +32,7 @@ const getChestSales = (walletList: Array<string>, mode: string) => {
   const params = new URLSearchParams();
   walletList.forEach((address) => params.append("walletList", address));
   params.append("mode", mode);
-  const response = fetch(
-    `https://198.199.79.252:8000/api/chest_sales?${params.toString()}`
-  );
+  const response = fetch(`/api/chest_sales?${params.toString()}`);
   return response;
 };
 
